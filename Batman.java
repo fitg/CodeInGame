b7236a865381669148bc5d44ca007d7a9451f092
fitg <gustaw.fit@gmail.com>
2019-10-20 16:45:15 +0100

    Update Batman.java

--- Batman.java
+++ Batman.java
@@ -15,93 +15,62 @@ class Player {
         int N = in.nextInt(); // maximum number of turns before game over.
         int X0 = in.nextInt();
         int Y0 = in.nextInt();
-        int X1 = X0;
-        int Y1 = Y0;
-        int[] mid = {W/2,H/2};
-        int i = 0;
-        int[] cur = {0,0};
-        int iterator = 1;
-
+        int X1 = 0;
+        int Y1 = 0;
+        int X2 = W-1;
+        int Y2 = H-1;
+        int[] cur = {X0,Y0};
+        
         // game loop
         while (true) {
             String bombDir = in.next(); // the direction of the bombs from batman's current location (U, UR, R, DR, D, DL, L or UL)
             // Write an action using System.out.println()
-            System.err.println(bombDir);
+            //System.err.println(bombDir);
+            //System.err.println( Y1 +" "+ Y2);
+            //System.err.println( X1 +" "+ X2);
             
-            if (H*W < 30) {
-                iterator = 1;
-            } else {
-                iterator = H/2;
+            switch (bombDir) {
+            case "U": 
+                Y2 = cur[1] - 1;
+                break;
+            case "UR":
+                Y2 = cur[1] - 1;
+                X1 = cur[0] + 1;
+                break;
+            case "UL":
+                Y2 = cur[1] - 1;
+                X2 = cur[0] - 1;
+                break;
+            case "R":
+                X1 = cur[0] + 1;
+                break;
+            case "DR":
+                Y1 = cur[1] + 1;
+                X1 = cur[0] + 1;
+                break;
+            case "D":
+                Y1 = cur[1] + 1;
+                X2 = cur[0] - 1;
+                break;
+            case "DL":
+                Y1 = cur[1] + 1;
+                X2 = cur[0] - 1;
+                break;
+            case "L":
+                X2 = cur[0] - 1;
+                break;
+            default: 
+                System.err.println("unexpected");
+                break;
             }
+            cur[0] = X1 + (X2 - X1) / 2;
+            cur[1] = Y1 + (Y2 - Y1) / 2;
             
-            System.err.println( iterator);
-            if(i == 0) {
-                
-                cur[0] = mid[0];
-                cur[1] = mid[1];
-                H = cur[1];
-                W = cur[0];
-            }
-            else {
-                switch (bombDir) {
-                case "U": 
-                    
-                    cur[1]-=iterator;
-                    H = cur[1];
-                    break;
-                case "UR":
-                    
-                    cur[0]+=iterator;
-                    cur[1]-=iterator;
-                    H = cur[1];
-                    W = cur[0];
-                    break;
-                case "R":
-                    
-                    cur[0]+=iterator;
-                    W = cur[0];
-                    break;
-                case "DR":
-                    
-                    cur[0]+=iterator;
-                    cur[1]+=iterator;
-                    W = cur[0];
-                    H = H - cur[1];
-                    break;
-                case "D":
-                    
-                    cur[1]+=iterator;
-                    H = H - cur[1];
-                    break;
-                case "DL":
-                    
-                    cur[0]-=iterator;
-                    cur[1]-=iterator;
-                    H = H - cur[1];
-                    W = W - cur[0];
-                    break;
-                case "L":
-                    
-                    cur[0]-=iterator;
-                    W = W - cur[0];
-                    break;
-                case "UL":
-                    
-                    cur[0]-=iterator;
-                    cur[1]-=iterator;
-                    W = W - cur[0];
-                    H = cur[1];
-                    break;
-                default: 
-                    cur[0] = mid[0];
-                    cur[1] = mid[1];
-                    break;
-                }
-            }
-            System.err.println( cur[0] +" "+ cur[1]);
+            //System.err.println((Y2 - Y1) / 2);
+            //System.err.println( cur[0] +" "+ cur[1]);
             // the location of the next window Batman should jump to.
             System.out.println( cur[0] +" "+ cur[1]);
-            i++;
+            
         }
     }
  
