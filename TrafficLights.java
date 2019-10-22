771e019b955579cce0af7be659574beaabcd8193
fitg <gustaw.fit@gmail.com>
2019-10-22 16:42:09 +0100

    Update TrafficLights.java

--- TrafficLights.java
+++ TrafficLights.java
@@ -1,6 +1,8 @@
 import java.util.*;
 import java.io.*;
 import java.math.*;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
 
 /**
  * Auto-generated code below aims at helping you parse
@@ -18,24 +20,52 @@ class Solution {
         int speedlimit = speed;
         int speedms = METER*speed/SECONDS;
         int lightCount = in.nextInt();
-        System.err.println(lightCount);
+        boolean fail = false;
+        Map<Integer, Integer> lightMap = new HashMap<>();
+        System.err.println("Speed: " + speed);
         for (int i = 0; i < lightCount; i++) {
             int distance = in.nextInt();
             int duration = in.nextInt();
-            if((distance/speedms) > duration){
-                speed = (int)(duration*MTOKM);
-            } else {
-                speed = speed;
-            }
-            
-            System.err.println(distance +" "+duration);
-            System.err.println(speedms);
+            lightMap.put(distance, duration);
         }
         
-        speed = (speed >= speedlimit ? speedlimit : speed); 
+        for(int i = speedlimit; i > 0; i--) {
+            
+            speedms = METER*roundUp(i,SECONDS);
+            fail = false;
+            
+            Iterator lightIterator = lightMap.entrySet().iterator();
+            
+            while (lightIterator.hasNext()) { 
+                Map.Entry light = (Map.Entry)lightIterator.next(); 
+                int distance = ((int)light.getKey()); 
+                int duration = ((int)light.getValue());
+                System.err.println("Speed: ["+ i + "]");
+                System.err.println("Distance: " + distance);
+                System.err.println("Duration: " + duration);
+                System.err.println("Speedms: " + speedms);
+                System.err.println("Lightspeed: " + distance/duration);
+                System.err.println("Modulo: " + (distance/duration)%speedms);
+                if(((distance/duration)%speedms != 0) && ((distance/duration)%speedms) < 10) {
+                    fail = true;
+                    System.err.println("setting to error");
+                }
+            }
+            System.err.println("Fail state: " + fail);
+            if (fail == false) {
+                speed = i;
+                break;
+            }
+        }
+        
+
         // Write an action using System.out.println()
         // To debug: System.err.println("Debug messages...");
-        System.err.println(speed);
+        //System.err.println(speed);
         System.out.println(speed);
+    }
+    
+    public static int roundUp(int num, int divisor) {
+        return (num + divisor - 1) / divisor;
     }
 }
