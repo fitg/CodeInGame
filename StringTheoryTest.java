c3e3a754b79e527729c5cc10e8aacc46ff6facbb
fitg <gustaw.fit@gmail.com>
2019-10-24 18:21:58 +0100

    Update StringTheoryTest.java

--- StringTheoryTest.java
+++ StringTheoryTest.java
@@ -1,1 +1,44 @@
 
+import java.util.*;
+import java.io.*;
+
+public class Solution_Test extends TestCase {
+	//
+	// /!\ Unit Tests are optional but highly recommended /!\
+	//
+
+	public void testCountLettersSimple() {
+		String p = "Hello World";
+		Map<String, Integer> types = Solution.countLetters(p);
+		
+		assertEquals((int)types.get(Solution.VOWELS), 3);
+		assertEquals((int)types.get(Solution.CONSONANTS), 7);
+	}
+	
+	public void testCountLettersComplex() {
+		String p = "Hello %^&%^%!%!%!World";
+		Map<String, Integer> types = Solution.countLetters(p);
+		
+		assertEquals((int)types.get(Solution.VOWELS), 3);
+		assertEquals((int)types.get(Solution.CONSONANTS), 7);
+	}
+	
+	public void testReverseAndToggle() {
+		String p = "Hello World";
+		
+		assertEquals(Solution.reverseWordsAndToggleCase(p), "wORLD hELLO");
+	}
+	
+	public void testInsertSpecial() {
+		String p = "oaeiu";
+		
+		assertEquals(Solution.insertSpecial(p), "pvopvapvepvipvu");
+	}
+	
+	public void testInsertSpecialCaseSens() {
+		String p = "Oaeiu";
+		
+		assertEquals(Solution.insertSpecial(p), "pvOpvapvepvipvu");
+	}
+
+}
